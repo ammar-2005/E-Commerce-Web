d28c9c08e1af0eacc4a008b60472eb69ef63d2fd
Your Name <you@example.com>
2026-02-05 10:48:20 +0200

    checknox-cart

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 let categorynavlist  = document.querySelector('.category-nav-list');
 function Opencategory(){
  categorynavlist.classList.toggle('active')
+
 }
+var cart = document.querySelector('.cart');
+function openCart(){
+    cart.classList.toggle('active')
+}
+  
 
